108b2e3953ebd31ddbabc1693c4d709cfcbbfae9
Matt Hadden <forceh2010@gmail.com>
2023-07-08 15:42:34 -0400

    feat: basic app/page/theme setup

---
+++ src/lib/theme/theme.ts
@@ -0,0 +1,30 @@
+import { createTheme } from "@mui/material";
+
+import { Overpass } from "@next/font/google";
+
+const overpass = Overpass({ subsets: ["latin"] });
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: overpass.style.fontFamily,
+    body1: { fontSize: "1.6rem" },
+    body2: { fontSize: "1.6rem" },
+    h1: { fontSize: "3.4rem" },
+    h2: { fontSize: "3.1rem" },
+    h3: { fontSize: "2.8rem" },
+    h4: { fontSize: "2.5rem" },
+    h5: { fontSize: "2.2rem" },
+    h6: { fontSize: "1.9rem" },
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        colorPrimary: {
+          backgroundColor: "#f9f9f9",
+          boxShadow: "0 2px 2px -2px rgba(0,0,0,.2);",
+          color: "#111",
+        },
+      },
+    },
+  },
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material";
+import Container from "@mui/material/Container";
+import { theme } from "lib/theme";
+import "../styles/reset.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
+      </Head>
+      <Container sx={{ paddingY: 3 }}>
+        <Component {...pageProps} />
+      </Container>
+    </ThemeProvider>
+  );
 }
