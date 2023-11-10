dd344bddafcddfd075cbda80234d37755ec312a4
Matt Hadden <forceh2010@gmail.com>
2023-11-09 19:03:35 -0500

    chore: rename tsx to ts where appropriate

--- src/lib/results/results.ts
+++ src/lib/results/results.ts
@@ -3,8 +3,8 @@ import { ResultEntry } from "types";
 
 export function sortResults(results: ResultEntry[]) {
   return results.sort((a: ResultEntry, b: ResultEntry) => {
-    const { finished: aFinished, laps: aLaps, time: aTime } = a;
-    const { finished: bFinished, laps: bLaps, time: bTime } = b;
+    const { finished: aFinished, laps: aLaps = 0, time: aTime } = a;
+    const { finished: bFinished, laps: bLaps = 0, time: bTime } = b;
 
     // sort by laps
     if (aLaps > bLaps) return -1;
