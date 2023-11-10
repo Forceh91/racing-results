90008b7f5637828b6879d3e81b742250d8043d0b
Matt Hadden <forceh2010@gmail.com>
2023-11-10 14:40:22 -0500

    chore: add tanstack query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,24 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "@mui/material";
 import Container from "@mui/material/Container";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { theme } from "lib/theme";
 import "../styles/reset.scss";
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
-      </Head>
-      <Container sx={{ paddingY: 3 }}>
-        <Component {...pageProps} />
-      </Container>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
+        </Head>
+        <Container sx={{ paddingY: 3 }}>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
